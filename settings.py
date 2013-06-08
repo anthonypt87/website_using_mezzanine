cf39164f011f20ffd56b9cb4675c2da0f55de918
anthony <anthonypt87@gmail.com>
2013-06-07 22:49:37 -0700

    Adding procfile and getting ready to deploy to heroku

--- settings.py
+++ settings.py
@@ -350,3 +350,11 @@ except ImportError:
     pass
 else:
     set_dynamic_settings(globals())
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
